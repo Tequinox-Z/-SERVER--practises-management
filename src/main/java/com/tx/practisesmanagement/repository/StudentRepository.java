a35d753207b84ff09b5d9f27fe23fb0a9e5423ee
tequinox <tequinox@unknown>
2022-05-12 09:26:06 +0200

    Cambiado BBDD

--- src/main/java/com/tx/practisesmanagement/repository/StudentRepository.java
+++ src/main/java/com/tx/practisesmanagement/repository/StudentRepository.java
@@ -3,6 +3,7 @@ package com.tx.practisesmanagement.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.tx.practisesmanagement.model.Student;
 
@@ -11,6 +12,7 @@ import com.tx.practisesmanagement.model.Student;
  * Repositorio de estudiantes
  * @author Salva
  */
+@Repository
 public interface StudentRepository extends JpaRepository<Student, String>{
 
 }
