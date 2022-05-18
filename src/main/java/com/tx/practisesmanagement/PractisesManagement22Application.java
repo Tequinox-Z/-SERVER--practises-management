e18b9884a6216e5543df552525b4b054655c2303
tequinox <tequinox@unknown>
2022-05-18 21:55:40 +0200

    Convertido en microservicio

--- src/main/java/com/tx/practisesmanagement/PractisesManagement22Application.java
+++ src/main/java/com/tx/practisesmanagement/PractisesManagement22Application.java
@@ -5,7 +5,9 @@ import java.util.Properties;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
