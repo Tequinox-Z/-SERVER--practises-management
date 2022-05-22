90471d6cea822d8fe9b6aec1b18eb6ba4f22e947
tequinox <tequinox@unknown>
2022-05-22 17:11:10 +0200

    * Creado todos los repositorios, actualizados modelos, actualizado Cross-Origin y más

--- src/main/java/com/tx/practisesmanagement/controller/AditionalsFunctionsController.java
+++ src/main/java/com/tx/practisesmanagement/controller/AditionalsFunctionsController.java
@@ -43,7 +43,7 @@ import com.tx.practisesmanagement.service.TeacherService;
  * @author Salva
  */
 @RestController
-@CrossOrigin(origins = "https://tequinox-z.github.io")
+@CrossOrigin(origins = "https://tequinox-z.github.io/Proyecto-Practises-/")
 public class AditionalsFunctionsController {
 	
 	// Servicios
